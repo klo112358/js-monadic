3a98584a703dfe9f9c51792aff90f760ab13bb33
klo112358 <klo112358@gmail.com>
2020-09-05 22:33:55 +0800

    Simplify code

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,14 @@ export default interface Monad<T, R> {
 export default function Monad<T, R>(unit: (value: T) => R, bind: (monad: R, fn: (value: T) => R) => R): Monad<T, R> {
   const obj = function(value: T): R { return unit(value) }
   obj.do = (gen: Generator<R, R, T>): R => {
-    const result = gen.next()
-    if (result.done) {
-      return result.value
-    }
-    function step(value: T) {
-      const result = gen.next(value)
+    function step(value?: T) {
+      const result = gen.next(value as T)
       if (result.done) {
         return result.value
       }
       return bind(result.value, step)
     }
-    return bind(result.value, step)
+    return step()
   }
   return obj
 }
